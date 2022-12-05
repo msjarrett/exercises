dcba31ddffb52403ce30ff38a83449ba6a68c33c
Michael Jarrett <michael@jarrettnw.com>
2022-12-04 23:04:05 -0800

    Day 5

---
+++ adventofcode2022/app/src/main/kotlin/com/knottysoftware/exercises/adventofcode2022/Day5.kt
@@ -0,0 +1,67 @@
+package com.knottysoftware.exercises.adventofcode2022;
+
+import kotlin.collections.ArrayDeque
+
+typealias Crate = Char
+
+class Day5 : Exercise {
+    private data class Move(var count: Int, var source: Int, var dest: Int)
+
+    private lateinit var stacks: List<ArrayDeque<Crate>>
+    private lateinit var moves: List<Move>
+
+    private final val moveRegex = Regex("""move (\d+) from (\d+) to (\d+)""")
+    private final val stackListRegex = Regex("""^( \d )(  \d )+""")
+
+    override fun parse(lines: Sequence<String>) {
+        val lineList = lines.toList()
+        val iStacks = lineList.indexOfFirst { stackListRegex.matches(it) }
+        require(iStacks > 0)
+        val nStacks = (lineList[iStacks].length + 1) / 4
+
+        // Parse stacks
+        stacks = buildList<ArrayDeque<Crate>> {
+            repeat(nStacks) { add(ArrayDeque()) }
+        }
+        for (iLine in (iStacks - 1) downTo 0) {
+            val line = lineList[iLine]
+            for (iStack in 0 until nStacks) {
+                val iCrate = iStack * 4 + 1
+                require(line.length > iCrate)
+                if (line[iCrate] != ' ') {
+                    stacks[iStack].addLast(line[iCrate])
+                }
+            }
+        }
+
+        // Parse moves
+        moves = lineList.drop(iStacks + 2).map {
+            val (moveN, moveFrom, moveTo) = moveRegex.matchEntire(it)!!.destructured
+            Move(moveN.toInt(), moveFrom.toInt() - 1, moveTo.toInt() - 1)
+        }
+    }
+
+    override fun partOne(): Long {
+        // Execute moves.
+        val stacks = this.stacks.map { ArrayDeque(it) }
+        for (move in moves) {
+            repeat(move.count) {
+                stacks[move.dest].addLast(stacks[move.source].removeLast())
+            }
+        }
+
+        println(stacks.map { it.last() }.joinToString(separator = ""))
+        return 1
+    }
+
+    override fun partTwo(): Long {
+        val stacks = this.stacks.map { ArrayDeque(it) }
+        for (move in moves) {
+            stacks[move.dest].addAll(stacks[move.source].takeLast(move.count))
+            repeat(move.count) { stacks[move.source].removeLast() }
+        }
+
+        println(stacks.map { it.last() }.joinToString(separator = ""))
+        return 1
+    }
+}
